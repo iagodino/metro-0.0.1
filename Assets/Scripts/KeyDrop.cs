513814e7ef0fd35c335aa164827f486d1a5b97ac
iagodino <iagovs@gmail.com>
2020-02-29 16:28:16 -0300

    Key and Drop Update

---
+++ Assets/Scripts/KeyDrop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDrop : MonoBehaviour
+{
+
+    public Key key;
+
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = key.image;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        { 
+            Destroy(gameObject);
+        }
+    }
+}
